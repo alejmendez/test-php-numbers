244b88f80b730f29001319c67268ba287587630e
Alejandro Méndez <alejmendez.87@gmail.com>
2021-09-09 23:24:40 -0300

    Add logic for numbers

--- src/Numbers/Numbers.php
+++ src/Numbers/Numbers.php
@@ -5,25 +5,23 @@ namespace app\Numbers;
 class Numbers
 {
     protected $list = [];
+    protected $messages = [
+        3 => 'home',
+        5 => 'IT',
+        8 => 'Integraciones',
+    ];
     public function __construct($first, $last)
     {
         for ($i = $first; $i <= $last; $i++) {
-            $number = $i;
-            /*
-            Write a program that prints all the numbers from 1 to 100. However, 
-            for multiples of 3, instead of the number, print "home". For multiples 
-            of 5 print "IT". For numbers which are multiples of both 3 and 5, 
-            print "Integraciones"
-
-            But here's the catch: you can use only one `if`. No multiple branches, ternary operators or `else`.
-            */
-            if ($i % 3 === 0 && $i % 5 === 0) {
-                $number = 'Integraciones [' . ($i % 2) . ']';
-            } elseif ($i % 3 === 0) {
-                $number = 'home [' . ($i % 2) . ']';
-            } elseif ($i % 5 === 0) {
-                $number = 'IT [' . ($i % 2) . ']';
+            $index = 0;
+            $this->messages[0] = $i;
+            foreach ([3, 5] as $multiple) {
+                if (0 == $i % $multiple) {
+                    $index += $multiple;
+                }
             }
+
+            $number = $this->messages[$index];
             $this->list[] = $number;
         }
     }
